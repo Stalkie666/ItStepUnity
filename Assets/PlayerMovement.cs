59e99381e9442c2d237c61c5d875a8d3bd277a4c
Stalkie666 <hamatlama@gmail.com>
2025-02-04 20:00:53 +0100

    jednoduche animace

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,24 +1,49 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
       rb = GetComponent<Rigidbody2D>();
+      anim = GetComponent<Animator>();
+      sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         float dirX = Input.GetAxisRaw("Horizontal");
-        rb.linearVelocity = new Vector2(dirX * 7f, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(dirX * moveSpeed, rb.linearVelocity.y);
+        
+        // fixing multiple jumping
+        if( Math.Abs(rb.linearVelocity.y) < .1f && Input.GetButtonDown("Jump") ){
+            rb.linearVelocity = new Vector2(dirX * moveSpeed, jumpForce);
+        }
+        UpdateAnimationMovement(dirX);
 
-        if( Input.GetButtonDown("Jump") ){
-            rb.linearVelocity = new Vector2(dirX * 7f,7);
-        }  
+    }
+
+    private void UpdateAnimationMovement(float dirX){
+        if( dirX > 0f ){
+            anim.SetBool("running",true);
+            sprite.flipX = false;
+        }
+        else if( dirX < 0f ){
+            anim.SetBool("running",true);
+            sprite.flipX = true;
+        }
+        else{
+            anim.SetBool("running",false);
+        }
     }
 }
